56599e3ca73520dcc9a857676cdefa23a74b5991
Cizaquita <cristian.izaquita@gmail.com>
2018-08-04 22:20:57 -0500

    Agregadas entidades al modelo, template AdminLTE

--- MiSangre/donaciones/admin.py
+++ MiSangre/donaciones/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Departamento)
+admin.site.register(Ciudad)
+admin.site.register(Identificacion)
+admin.site.register(Donante)
+admin.site.register(Banco)
+admin.site.register(Inventario)
+admin.site.register(Donacion)
--- MiSangre/donaciones/models.py
+++ MiSangre/donaciones/models.py
@@ -7,15 +7,20 @@ class Departamento(models.Model):
 	def __str__(self):
 		return self.nombre
 
-# Se crea la ciudad para poblar la base de datos
-class Ciudade(models.Model):
+class Ciudad(models.Model):
 	departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
 	nombre = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.nombre + ' - ' + self.departamento.nombre
-		
-# Modelo proncipal, CIUDADANO
+
+# Se crea un modelo de tipo de identificacion
+class Identificacion(models.Model):
+	nombre = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.nombre
+# Modelo principal, Donante
 class Donante(models.Model):
 	GRUPOS_SANGUINEOS = (
         ('Positivo', 'Positivo'),
@@ -27,7 +32,6 @@ class Donante(models.Model):
         ('AB', 'AB'),
         ('O', 'O'),
     )
-
 	nombres = models.CharField(max_length=200)
 	apellidos = models.CharField(max_length=200)
 	tipo_identificacion = models.ForeignKey(Identificacion, on_delete=models.CASCADE)
@@ -45,31 +49,43 @@ class Donante(models.Model):
 	def __str__(self):
 		return self.nombres + ' ' + self.apellidos
 
-
 class Banco(models.Model):
 	nombre = models.CharField(max_length=50,unique=True)
 	categoria = models.CharField(max_length=2)
-	ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE, related_name='banco_ciudad')
+	ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
 	localidad = models.CharField(max_length=75)
 	codigo_nacional_sangre = models.CharField(max_length=10,unique=True)
-	lat = models.FloatField(validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)])
-    lon = models.FloatField(validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)])
+	lat = models.FloatField()
+	lon = models.FloatField()
 	direccion = models.CharField(max_length=50)
 	"""docstring for Banco"""
 	def __str__(self):
 		return self.nombre
 
+class Inventario(models.Model):
+	GRUPOS_SANGUINEOS = (
+        ('Positivo', 'Positivo'),
+        ('Negativo', 'Negativo'),
+    )
+	TIPOS_RH = (
+        ('A', 'A'),
+        ('B', 'B'),
+        ('AB', 'AB'),
+        ('O', 'O'),
+    )
+	banco = models.ForeignKey(Banco, on_delete=models.CASCADE)
+	rh = models.CharField(max_length=3, choices=TIPOS_RH, unique=True)
+	grupo_sanguineo = models.CharField(max_length=9, choices=GRUPOS_SANGUINEOS, unique=True)
+	cantidad_sangre = models.FloatField(default=0.0)
+	"""docstring for Inventario"""
+	def __str__(self):
+		return self.banco + ' ' + self.rh + self.grupo_sanguineo
 
 
-#class UnidadMovil(models.Model):
-#	banco = models.ForeignKey(Banco, on_delete=models.CASCADE, related_name='unidad_banco')
-#	nombre = models.CharField(max_length=50)
-#	ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE, related_name='unidad_ciudad')
-#	codigo_nacional_sangre = models.CharField(max_length=10,unique=True)
-#	lat = models.FloatField(validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)])
-#    lon = models.FloatField(validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)])
-#	direccion = models.CharField(max_length=50)
-#	"""docstring for Banco"""
-#	def __str__(self):
-#		return self.nombre
-
+class Donacion(models.Model):
+	donante = models.ForeignKey(Donante, on_delete=models.CASCADE)
+	banco = models.ForeignKey(Banco, on_delete=models.CASCADE)
+	cantidad_sangre = models.FloatField(default=450.0)
+	"""docstring for Donacion"""
+	def __str__(self):
+		return self.banco.nombre + ' - ' + self.donante.nombres
